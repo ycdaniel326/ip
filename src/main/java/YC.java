64dcd3ac6b474adf4cd43b209131217e8202133f
Daniel <ycdaniel326@gmail.com>
2025-08-26 02:29:33 +0800

    Renamed the filename to be YC and changed the bot name to YC. Added codes for greeting and exitting

---
+++ src/main/java/YC.java
@@ -0,0 +1,8 @@
+public class YC {
+    public static void main(String[] args) {
+        String bot_Name = "YC";
+        System.out.println("Hello! I'm " + bot_Name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
