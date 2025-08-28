ba490dfb1b9210602c0b430662866a13ca53f9dd
Daniel <ycdaniel326@gmail.com>
2025-08-28 16:02:43 +0800

    Added the list to store users' commands and ability to list out the commands.

--- src/main/java/YC.java
+++ src/main/java/YC.java
@@ -4,6 +4,8 @@ public class YC {
         String bot_Name = "YC";
         Scanner scanner = new Scanner(System.in);
         String userCommand;
+        String[] commandList = new String[100];
+        int totalCommands = 0;
 
         System.out.println("\t" + "____________________________________________________________");
         System.out.println("\t" + "Hello! I'm " + bot_Name);
@@ -18,9 +20,20 @@ public class YC {
                 System.out.println("\t" + "____________________________________________________________" + "\n");
                 break;
             }
-            System.out.println("\t" + "____________________________________________________________");
-            System.out.println("\t" + userCommand + "\n");
-            System.out.println("\t" + "____________________________________________________________" + "\n");
+            if (userCommand.equalsIgnoreCase("list")) {
+                System.out.println("\t" + "____________________________________________________________");
+                for  (int i = 0; i < totalCommands; i++) {
+                    System.out.println("\t" + (i+1) + ". "+ commandList[i]);
+                }
+                System.out.println("\t" + "____________________________________________________________" + "\n");
+            }
+            else {
+                commandList[totalCommands] = userCommand;
+                totalCommands++;
+                System.out.println("\t" + "____________________________________________________________");
+                System.out.println("\t" + "added: " + userCommand + "\n");
+                System.out.println("\t" + "____________________________________________________________" + "\n");
+            }
 
         }
     }
