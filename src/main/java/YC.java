21f52eb1b25ac67fee209055cd8e4e8976b94b3c
Daniel <ycdaniel326@gmail.com>
2025-08-28 15:02:23 +0800

    Added Level 1-1 Echo

--- src/main/java/YC.java
+++ src/main/java/YC.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
 public class YC {
     public static void main(String[] args) {
         String bot_Name = "YC";
-        System.out.println("Hello! I'm " + bot_Name);
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        String userCommand;
+
+        System.out.println("\t" + "____________________________________________________________");
+        System.out.println("\t" + "Hello! I'm " + bot_Name);
+        System.out.println("\t" + "What can I do for you?");
+        System.out.println("\t" + "____________________________________________________________" + "\n");
+
+        while (true) {
+            userCommand = scanner.nextLine();
+            if (userCommand.equalsIgnoreCase("bye")) {
+                System.out.println("\t" + "____________________________________________________________");
+                System.out.println("\t" + "Bye. Hope to see you again soon!");
+                System.out.println("\t" + "____________________________________________________________" + "\n");
+                break;
+            }
+            System.out.println("\t" + "____________________________________________________________");
+            System.out.println("\t" + userCommand + "\n");
+            System.out.println("\t" + "____________________________________________________________" + "\n");
+
+        }
     }
 }
